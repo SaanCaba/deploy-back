6eea8bbb55e2859a4f4eb9ce27765327c3fb914f
Santiago Caballero <saantyc12.caballero@gmail.com>
2022-12-01 18:34:25 -0300

    dsad

--- src/routes/authPs.ts
+++ src/routes/authPs.ts
@@ -7,32 +7,31 @@ const mongoose = require('mongoose')
 
 router.get("/login/success", async(req: any, res: Response) => {
     console.log(req)
-    return res.send('hola')
-    // if (req.user) {
-	// 	res.status(200).json({
-	// 		error: false,
-	// 		message: "Successfully Loged In",
-	// 		user: req.user,
-	// 		token: generateAuthToken()
-	// 	});
-    //     console.log(req.user._json)
+    if (req.user) {
+		res.status(200).json({
+			error: false,
+			message: "Successfully Loged In",
+			user: req.user,
+			token: generateAuthToken()
+		});
+        console.log(req.user._json)
 
-    //     const user = await User.findOne({email: req.user._json.email})
+        const user = await User.findOne({email: req.user._json.email})
         
-    //     if(!user){
-    //         console.log('entre')
-    //         await new User({
-    //             firstName: req.user._json.firstName || req.user._json.given_name,
-    //             lastName: req.user._json.lastName || req.user._json.family_name,
-    //             email:req.user._json.email
-    //         }).save()
-    //         console.log('creado')
-    //     }
-    //     console.log('opa')
+        if(!user){
+            console.log('entre')
+            await new User({
+                firstName: req.user._json.firstName || req.user._json.given_name,
+                lastName: req.user._json.lastName || req.user._json.family_name,
+                email:req.user._json.email
+            }).save()
+            console.log('creado')
+        }
+        console.log('opa')
         
-	// } else {
-	// 	res.status(403).json({ error: true, message: "Not Authorized" });
-	// }
+	} else {
+		res.status(403).json({ error: true, message: "Not Authorized", data: req });
+	}
 });
 
 router.get("/login/failed", (req: Request, res: Response) => {
